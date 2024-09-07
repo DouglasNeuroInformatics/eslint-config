98a1598eb2473507354a613158f653a57f092e90
joshunrau <contact@joshuaunrau.com>
2024-09-06 20:37:54 -0400

    fix: allow require as import

--- src/configs/typescript.ts
+++ src/configs/typescript.ts
@@ -59,7 +59,12 @@ export const typescriptConfig = async ({
         '@typescript-eslint/no-namespace': 'error',
         '@typescript-eslint/no-non-null-asserted-optional-chain': 'error',
         '@typescript-eslint/no-redundant-type-constituents': 'error',
-        '@typescript-eslint/no-require-imports': 'error',
+        '@typescript-eslint/no-require-imports': [
+          'error',
+          {
+            allowAsImport: true
+          }
+        ],
         '@typescript-eslint/no-this-alias': 'error',
         '@typescript-eslint/no-unnecessary-type-assertion': 'error',
         '@typescript-eslint/no-unnecessary-type-constraint': 'error',
